3acabedeb3c3232d32fbc4ad112cc717b8a917b5
Adrienne Dao <adriennedao@gmail.com>
2016-12-18 04:56:18 +0000

    Updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts, only: :create
-  get 'contact-us', to: 'contact#new', as: 'new_contact'
+  resources :contacts
+  
 end
