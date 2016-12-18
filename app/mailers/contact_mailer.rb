6b6dd8d0c0fa07a29cd560c8c1fc6345ee3b52c6
Adrienne Dao <adriennedao@gmail.com>
2016-12-18 22:07:36 +0000

    modified files

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'adriennedao@gmail.com'
     
-    def email_contact(name, email, body)
+    def contact_email(name, email, body)
       @name = name
       @email= email
       @body = body
